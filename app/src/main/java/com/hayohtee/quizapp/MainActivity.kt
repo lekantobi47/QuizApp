e4c26bec509a86ad5cdbbc299807611650793500
Olamilekan Akintilebo <lekantobi47@gmail.com>
2021-03-02 15:37:42 +0100

    Updated MainActivity.kt

--- app/src/main/java/com/hayohtee/quizapp/MainActivity.kt
+++ app/src/main/java/com/hayohtee/quizapp/MainActivity.kt
@@ -2,10 +2,34 @@ package com.hayohtee.quizapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
+import com.hayohtee.quizapp.databinding.ActivityMainBinding
+import com.hayohtee.quizapp.model.QuestionViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private val questionViewModel: QuestionViewModel by lazy {
+        ViewModelProvider(this).get(QuestionViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        displayQuestion()
+        setHandlers()
     }
+
+    private fun displayQuestion(){
+        val question = questionViewModel.getQuestion()
+        binding.questionTextView.text = question
+    }
+
+    private fun setHandlers(){
+        binding.nextButton.setOnClickListener {
+            questionViewModel.nextQuestion()
+            displayQuestion()
+        }
+    }
+
 }
